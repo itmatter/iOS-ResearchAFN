600b367b1bfb1bdb9204e1281c31e0e344d1b156
Li_guang <liliguang@liliguangdeMac-mini.local>
2017-03-24 10:25:08 +0800

    Pod AFN
    pod install AFN

---
+++ ResearchAFN/ResearchAFN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ResearchAFN
+//
+//  Created by 李礼光 on 2017/3/24.
+//  Copyright © 2017年 李礼光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
